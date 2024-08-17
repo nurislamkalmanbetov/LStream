a11e5a1dedc2c2be52a21b619af9fe56df9a535e
nurislamkalmanbetov <kalmanbetovnurislam19@gmail.com>
2024-08-17 12:55:55 +0600

    add game_live

--- apps/games/views.py
+++ apps/games/views.py
@@ -7,11 +7,12 @@ class GameListView(ListView):
     model = Games 
     template_name = 'pages/game_list.html'
     context_object_name = 'games'
-    queryset = Games.objects.all()
+    queryset = Games.objects.all().order_by('-created_at')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category'] = Category.objects.all()
+        # context['category'] = Category.objects.all()
+        context['top_down'] = Games.objects.order_by('-download_count')[:6]
         return context
     
 class GamesDetail(DetailView):
